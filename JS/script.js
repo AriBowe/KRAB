9c3836130b0eb4a61c54c5190be18648449cc8ac
Ari Bowe <ari.otis.bowe@gmail.com>
2019-09-08 18:29:11 +1000

    Uploaded the right files and make the css prettier

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,13 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
-  document.getElementById("mySidebar").style.width = "250px";
-  document.getElementById("open").style.marginRight = "250px";
+  document.getElementById("mySidebar").style.width = "15vw";
+  document.getElementById("open").style.marginRight = "15vw";
+  document.getElementsByClassName("openbtn")[0].setAttribute("onclick", "closeNav()");
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("open").style.marginRight = "0";
+  document.getElementsByClassName("openbtn")[0].setAttribute("onclick", "openNav()");
 }
