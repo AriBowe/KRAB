819c35fc48cb38b3dc6da758cc2c168814d2b5bb
unknown <u6679094@anu.edu.au>
2019-10-20 16:23:43 +1000

    image update + edits

--- game.php
+++ game.php
@@ -8,7 +8,7 @@ include 'statusbar.php';
 <main>
     <div id="event">
         <div id="character">
-            <img src="images/no_profile.jpg" alt="">   
+            <img src="images/convict3.jpg" alt="">   
             <h2>Character Name</h2>
         </div>
         <p>You're a poor Englishman in the 19th century. Click the buttons below to make decisions and go about your day.</p>
@@ -26,7 +26,7 @@ include 'statusbar.php';
             <p>Not bought</p>
         </div>
     </div> 
-    <h2>Choose a character</h2>
+    <!--<h2>Choose a character</h2>-->
     <div id="characterSelect">
 
         <div class="characterOption">
