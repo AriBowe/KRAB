106829a227d17718b933598f6a8591da3b903f1e
unknown <u6679094@anu.edu.au>
2019-10-21 15:54:44 +1000

    edits

--- data.php
+++ data.php
@@ -25,17 +25,7 @@ include 'sidebar.php';
 		
 		</article>
 
-<!-- <article class="record record-template">
-        <h2>Record Name</h2>
-        <br>
-        <h3>Record Detail</h3>
 
-</article>
-
-<script src="js/jquery-3.4.1.min.js"></script>
-<script src="js/strip.pkgd.min.js"></script>
-<script src="js/script.js"></script>
-	</main> -->
 </main>
 
 		
